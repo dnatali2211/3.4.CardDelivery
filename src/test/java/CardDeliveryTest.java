8b0ac08696395cd0bebf4bb8baf6ffab880d9b3c
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-06-26 15:56:56 +0300

    Added failed test

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    private String generateDate(int daysToAdd) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate deliveryDate = currentDate.plusDays(daysToAdd);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String deliveryDateStr = deliveryDate.format(formatter);
+        return deliveryDateStr;
+    }
+
+    @Test
+    void shouldDeliveryCard() throws InterruptedException {
+        open("http://localhost:9999");
+
+        $("[data-test-id=city] input").setValue("Ростов-на-Дону");
+        $("[data-test-id=date] input").doubleClick().sendKeys(generateDate(4));
+        $("[data-test-id=name] input").setValue("Фёдорова Алла-Светлана");
+        $("[data-test-id=phone] input").setValue("+79013749921");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(13));
+        $(withText("Успешно")).shouldBe(Condition.visible);
+        Thread.sleep(10000);
+    }
+}
